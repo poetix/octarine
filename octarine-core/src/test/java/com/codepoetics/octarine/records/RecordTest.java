32ef0be835b82e9f2dbca51603d69c38a507f301
Dave Snowdon <dave@davesnowdon.com>
2017-05-13 16:11:10 +0100

    Support lenses on valid records
    Suggestion by Dominic Fox on how to support lenses on ValidRecords.
    
    Added suggested code by Dominic Fox and tests to confirm that it words as expected.

--- octarine-core/src/test/java/com/codepoetics/octarine/records/RecordTest.java
+++ octarine-core/src/test/java/com/codepoetics/octarine/records/RecordTest.java
@@ -2,12 +2,20 @@ package com.codepoetics.octarine.records;
 
 import com.codepoetics.octarine.functional.functions.F2;
 import com.codepoetics.octarine.functional.lenses.Lens;
+import com.codepoetics.octarine.records.example.Address;
+import com.codepoetics.octarine.records.example.Person;
 import org.junit.Test;
+import org.pcollections.PVector;
+import org.pcollections.TreePVector;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Function;
 
-import static com.codepoetics.octarine.Octarine.*;
+import static com.codepoetics.octarine.Octarine.$;
+import static com.codepoetics.octarine.Octarine.$$;
+import static com.codepoetics.octarine.Octarine.$L;
+import static com.codepoetics.octarine.Octarine.$R;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -28,10 +36,19 @@ public class RecordTest {
             )
     );
 
-    @Test public void
+    private static final Record validTestRecord = $$(
+            Person.name.of("Tom Cat"),
+            Person.age.of(9),
+            Person.address.of(
+                    Address.addressLines.of("1 Cat Road", "Smallville", "T11 M22")
+            )
+    );
+
+    @Test
+    public void
     getKeysFromRecord() {
         assertThat(testRecord.get(address).flatMap(address ->
-                address.get(addressLines)).get(),
+                        address.get(addressLines)).get(),
                 hasItems("23 Acacia Avenue", "Sunderland", "VB6 5UX"));
     }
 
@@ -55,6 +72,38 @@ public class RecordTest {
 
     @Test
     public void
+    keys_are_lenses_on_valid_records() {
+        Lens<Record, PVector<String>> linesLens =
+                Person.address.assertPresent()
+                        .join(Address.addressLines.assertValidPresent(Address.schema));
+
+        assertThat(linesLens.get(validTestRecord), equalTo(Arrays.asList("1 Cat Road", "Smallville", "T11 M22")));
+        assertThat(linesLens.set(validTestRecord, TreePVector.from(Arrays.asList("1 Mouse Street", "Mouseville", "M11 T22"))),
+                equalTo($$(
+                        Person.name.of("Tom Cat"),
+                        Person.age.of(9),
+                        Person.address.of(
+                                Address.addressLines.of("1 Mouse Street", "Mouseville", "M11 T22")
+                        )
+                ))
+        );
+
+        Lens<Record, String> secondLineOfValidAddress = linesLens.join(Lens.intoPVector(1));
+
+        assertThat(secondLineOfValidAddress.get(validTestRecord), equalTo("Smallville"));
+        assertThat(secondLineOfValidAddress.set(validTestRecord, "Big Town"),
+                equalTo($$(
+                        Person.name.of("Tom Cat"),
+                        Person.age.of(9),
+                        Person.address.of(
+                                Address.addressLines.of("1 Cat Road", "Big Town", "T11 M22")
+                        )
+                ))
+        );
+    }
+
+    @Test
+    public void
     keys_are_extractors() {
         assertThat(address.test(testRecord), equalTo(true));
         assertThat(notAppearingInThisRecord.test(testRecord), equalTo(false));
@@ -64,7 +113,8 @@ public class RecordTest {
         assertThat(notAppearingInThisRecord.get(testRecord), equalTo(Optional.empty()));
     }
 
-    @Test public void
+    @Test
+    public void
     reading_from_a_value() {
         Key<Character> firstChar = $("firstChar");
         Key<Character> secondChar = $("secondChar");
